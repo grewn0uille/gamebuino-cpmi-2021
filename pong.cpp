83d61e7c80945dc6837816c4be88d99764221c77
Lucie Anglade <lucie@courtbouillon.org>
2021-02-24 15:58:22 +0100

    On peut simplifier les ifs qui gèrent la sortie de balle de l’écran

--- pong.cpp
+++ pong.cpp
@@ -47,18 +47,11 @@ void loop() {
     balle_speedY = -1;
   }
 
-  if (balle_posX < 0) {
+  if ((balle_posX < 0) || (balle_posX > gb.display.width())){
     balle_posX = 20;
     balle_posY = 20;
     balle_speedX = 1;
     balle_speedY = 1;
-  }
-  if (balle_posX > gb.display.width()) {
-    balle_posX = 20;
-    balle_posY = 20;
-    balle_speedX = 1;
-    balle_speedY = 1;
-  }
 
   gb.display.fillRect(balle_posX, balle_posY, balle_taille, balle_taille);
   gb.display.fillRect(raquette1_posX, raquette1_posY, raquette_largeur, raquette_hauteur);
