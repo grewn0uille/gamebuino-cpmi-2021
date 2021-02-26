6c542594b0cd753dc63fcd11efaf54f109afd53f
Lucie Anglade <lucie@courtbouillon.org>
2021-02-26 11:27:01 +0100

    Des commentaires dans le code, c’est toujours sympa

--- pong.cpp
+++ pong.cpp
@@ -29,56 +29,70 @@ void loop() {
   balle_posX = balle_posX + balle_speedX;
   balle_posY = balle_posY + balle_speedY;
 
+  // Si on reste appuyé vers le haut, la raquette monte
   if (gb.buttons.repeat(BUTTON_UP, 0)){
     raquette1_posY = raquette1_posY - 1;
   }
+  // Si on reste appuyé vers le bas, la raquette descend
   if (gb.buttons.repeat(BUTTON_DOWN, 0)){
     raquette1_posY = raquette1_posY + 1;
   }
 
+  // Si on reste appuyé sur B, la raquette monte
   if (gb.buttons.repeat(BUTTON_B, 0)){
     raquette2_posY = raquette2_posY - 1;
   }
+  // Si on reste appuyé sur A, la raquette descend
   if (gb.buttons.repeat(BUTTON_A, 0)){
     raquette2_posY = raquette2_posY + 1;
   }
 
+  // Si la balle touche le haut de l’écran, on la fait aller vers le bas
   if (balle_posY < 0){
     balle_speedY = 1;
   }
+  // Si la balle touche le bas de l’écran, on la fait aller vers le haut
   if (balle_posY > gb.display.height() - balle_taille){
     balle_speedY = -1;
   }
 
+  // Si la balle sort à droite ou à gauche de l’écran
   if ((balle_posX < 0) || (balle_posX > gb.display.width())){
+    // Si elle sort à gauche, le joueur 2 gagne un point
     if (balle_posX < 0){
         score2 = score2 + 1;
     }
+    // Si elle sort à droite, le joueur 1 gagne un point
     if (balle_posX > gb.display.width()){
         score1 = score1 + 1;
     }
+    // On replace la balle
     balle_posX = 20;
     balle_posY = 20;
     balle_speedX = 1;
     balle_speedY = 1;
   }
 
+  // Si la balle tape contre le bord droit de la raquette de gauche, on la renvoie vers la droite
   if ((balle_posX == raquette1_posX + raquette_largeur)
       && (balle_posY + balle_taille >= raquette1_posY)
       && (balle_posY <= raquette1_posY + raquette_hauteur)){
     balle_speedX = 1;
   }
+  // Si la balle tape contre le bord gauche de la raquette de droite, on la renvoie vers la gauche
   if ((balle_posX == raquette2_posX - raquette_largeur)
       && (balle_posY + balle_taille >= raquette2_posY)
       && (balle_posY <= raquette2_posY + raquette_hauteur)){
     balle_speedX = -1;
   }
 
+  // On affiche les score en haut de l’écran
   gb.display.setCursor(35, 5);
   gb.display.print(score1);
   gb.display.setCursor(42, 5);
   gb.display.print(score2);
 
+  // On affiche la balle et les raquettes
   gb.display.fillRect(balle_posX, balle_posY, balle_taille, balle_taille);
   gb.display.fillRect(raquette1_posX, raquette1_posY, raquette_largeur, raquette_hauteur);
   gb.display.fillRect(raquette2_posX, raquette2_posY, raquette_largeur, raquette_hauteur);
